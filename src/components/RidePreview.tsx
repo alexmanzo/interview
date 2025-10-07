a9bb524da91406edd3447ea8bb8b98e25960bbd4
Alex Manzo <manzoalex17@gmail.com>
2025-10-07 15:55:02 -0400

    Implement sorting

--- src/components/RidePreview.tsx
+++ src/components/RidePreview.tsx
@@ -1,56 +1,46 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import type { EntityLiveData } from '@/types/parks-api';
 import { useMemo } from 'react';
 import { Badge } from './ui/badge';
-import spacetime from 'spacetime';
 
-export default function RidePreview({ entity }: { entity: EntityLiveData }) {
-  const waitTime = entity.queue?.STANDBY?.waitTime ?? '-';
+interface RidePreviewProps {
+  ride: EntityLiveData & { waitTime: number | null; lastUpdatedMinutes: number };
+}
 
-  const updatedTimeAgo = useMemo(() => {
-    const lastUpdated = spacetime(entity.lastUpdated);
-    const now = spacetime.now();
-    const diff = lastUpdated.diff(now);
-    if (diff.hours > 0) return `${diff.hours}h ago`;
-    if (diff.minutes > 0) return `${diff.minutes}m ago`;
-    return 'Just now';
-  }, [entity.lastUpdated]);
-  ``;
+export default function RidePreview({ ride }: RidePreviewProps) {
+  const lastUpdated = useMemo(() => {
+    if (ride.lastUpdatedMinutes === 0) return 'Just now';
+    if (ride.lastUpdatedMinutes === 1) return '1 minute ago';
+    if (ride.lastUpdatedMinutes < 60) return `${ride.lastUpdatedMinutes} minutes ago`;
+    const hours = Math.floor(ride.lastUpdatedMinutes / 60);
+    return `${hours} hour${hours > 1 ? 's' : ''}  ago`;
+  }, [ride.lastUpdatedMinutes]);
   const defaultWaitClasses = cn(['bg-gray-100', 'text-gray-800']);
   const longWaitClasses = cn(['bg-red-100', 'text-red-800']);
   const mediumWaitClasses = cn(['bg-yellow-100', 'text-yellow-800']);
   const shortWaitClasses = cn(['bg-green-100', 'text-green-800']);
   const waitTimeClasses = useMemo(() => {
-    if (waitTime === '-') return defaultWaitClasses;
-    return waitTime > 60 ? longWaitClasses : waitTime > 30 ? mediumWaitClasses : shortWaitClasses;
-  }, [waitTime]);
+    if (!ride.waitTime) return defaultWaitClasses;
+    return ride.waitTime > 60 ? longWaitClasses : ride.waitTime > 30 ? mediumWaitClasses : shortWaitClasses;
+  }, [ride.waitTime]);
 
   return (
     <Card>
       <CardContent className="flex justify-between gap-3">
         <div className="inline-flex w-20 h-20">
           <span className={cn(waitTimeClasses, 'rounded-full w-full h-full flex flex-col justify-center items-center')}>
-            <span className="text-3xl font-medium">{waitTime}</span>
+            <span className="text-3xl font-medium">{ride.waitTime || '-'}</span>
             <span>min</span>
           </span>
         </div>
         <div className="flex flex-col gap-3 justify-center">
-          <CardTitle className="self-center text-right">{entity.name}</CardTitle>
+          <CardTitle className="self-center text-right">{ride.name}</CardTitle>
           <Badge variant="secondary" className="ml-auto">
-            {updatedTimeAgo}
+            {lastUpdated}
           </Badge>
         </div>
       </CardContent>
-
     </Card>
   );
 }
--- src/components/RideWaitTimeList.tsx
+++ src/components/RideWaitTimeList.tsx
@@ -1,14 +1,100 @@
-import { useQuery } from '@tanstack/react-query';
-import { getLiveData } from '@/api/parks';
 import RidePreview from './RidePreview';
 import type { EntityLiveData } from '@/types/parks-api';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { useMemo, useState } from 'react';
+import { ArrowDownIcon, ArrowUpIcon } from 'lucide-react';
+import { Button } from './ui/button';
+import spacetime from 'spacetime';
+
+type SortCategory = 'waitTime' | 'name' | 'lastUpdatedMinutes';
 
 export default function RideWaitTimeList({ liveData }: { liveData: Array<EntityLiveData> }) {
-  const openRides = liveData.filter((ride) => ride.status === 'OPERATING' && !!ride.queue?.STANDBY?.waitTime) || [];
+  const [sort, setSort] = useState<{ direction: 'asc' | 'desc'; category: SortCategory }>({
+    direction: 'asc',
+    category: 'lastUpdatedMinutes',
+  });
 
+  const rideData = useMemo(() => {
+    return liveData.map((ride) => {
+      const waitTime = ride.queue?.STANDBY?.waitTime || null;
+      const lastUpdated = spacetime(ride.lastUpdated);
+      const now = spacetime.now();
+      const diff = lastUpdated.diff(now);
+      let lastUpdatedMinutes = 0;
+      if (diff.hours > 0) lastUpdatedMinutes = diff.hours * 60;
+      if (diff.minutes > 0) lastUpdatedMinutes += diff.minutes;
+      return { ...ride, waitTime, lastUpdatedMinutes };
+    });
+  }, [liveData]);
+
+  const openRides = useMemo(
+    () => rideData.filter((ride) => ride.status === 'OPERATING' && !!ride.queue?.STANDBY?.waitTime) || [],
+    rideData
+  );
+
+  const sortedRides = useMemo(
+    () => {
+      return openRides.sort((a, b) => {
+        let aValue: number | string = 0;
+        let bValue: number | string = 0;
+        switch (sort.category) {
+          case 'waitTime':
+            aValue = a.waitTime || 0;
+            bValue = b.waitTime || 0;
+            break;
+          case 'name':
+            aValue = a.name.trim().toLocaleLowerCase().replace(`"`, '')
+            bValue = b.name.trim().toLocaleLowerCase().replace(`"`, '')
+            break;
+          case 'lastUpdatedMinutes':
+            aValue = a.lastUpdatedMinutes;
+            bValue = b.lastUpdatedMinutes;
+            break;
+          default:
+            aValue = a.lastUpdatedMinutes;
+            bValue = b.lastUpdatedMinutes;
+            break;
+        }
+        if (aValue < bValue) return sort.direction === 'asc' ? -1 : 1;
+        if (aValue > bValue) return sort.direction === 'asc' ? 1 : -1;
+        return 0;
+      });
+    },
+    [sort, openRides]
+  );
+
+  function onSortCategoryChange(value: string) {
+    setSort({ ...sort, category: value });
+  }
   return (
-    <div className="flex flex-col gap-4">
-      {openRides.length > 0 && openRides.map((item) => <RidePreview key={item.id} entity={item} />)}
-    </div>
+    <>
+      <div className="flex mb-3 justify-between items-center">
+        <h2>Wait times</h2>
+        <div className="flex gap-1.5 justify-between">
+          <Select value={sort.category} onValueChange={onSortCategoryChange} aria-label="Sort by">
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Sort by..." />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="waitTime">Wait time</SelectItem>
+              <SelectItem value="name">Name</SelectItem>
+              <SelectItem value="lastUpdatedMinutes">Last Updated</SelectItem>
+            </SelectContent>
+          </Select>
+          <Button
+            variant="outline"
+            size="icon"
+            aria-label={sort.direction === 'asc' ? 'Sort descending' : 'Sort ascending'}
+            onClick={() => setSort({ ...sort, direction: sort.direction === 'asc' ? 'desc' : 'asc' })}
+          >
+            {sort.direction === 'asc' && <ArrowUpIcon />}
+            {sort.direction === 'desc' && <ArrowDownIcon />}
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        {openRides.length > 0 && sortedRides.map((item) => <RidePreview key={item.id} ride={item} />)}
+      </div>
+    </>
   );
 }
