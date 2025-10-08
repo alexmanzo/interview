c780b7926cfb86e674959fbc8e52a3d9fc5b9134
Alex Manzo <manzoalex17@gmail.com>
2025-10-07 20:06:00 -0400

    Remove nitro, properly provide query client context to router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+import { defineConfig } from 'vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    nitroV2Plugin(),
-    tanstackRouter({ autoCodeSplitting: true, target: 'react', }),
-    viteReact(),
-    tailwindcss(),
-  ],
+  plugins: [tanstackRouter({ autoCodeSplitting: true, target: 'react' }), viteReact(), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-})
+});
